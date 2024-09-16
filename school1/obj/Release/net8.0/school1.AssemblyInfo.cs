743ef4cb12fad6929a5a8b03e2b73e649f90dd46
thom <thom.noppies@gmail.com>
2024-09-16 17:24:38 +0200

    test

--- school1/obj/Release/net8.0/school1.AssemblyInfo.cs
+++ school1/obj/Release/net8.0/school1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("school1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893c8ffd9b56f4d61f4b2b962959a8e3482c1596")]
 [assembly: System.Reflection.AssemblyProductAttribute("school1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("school1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
